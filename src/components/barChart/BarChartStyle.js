f3365e7ab1530b2db2721965f482116a8da54d90
Alexandr <litovchenkoalexandr1992@gmail.com>
2023-02-01 11:55:33 +0200

    Added gh-pages Fixed border for chart

--- src/components/barChart/BarChartStyle.js
+++ src/components/barChart/BarChartStyle.js
@@ -10,7 +10,9 @@ export const breakpointStyles = {
     },
 
     diagramItem: {
-      border: "1px solid black",
+      borderTop: "1px solid black",
+      borderRight: "1px solid black",
+      borderBottom: "1px solid black",
       borderLeft: "none",
       height: "30px",
     },
@@ -30,7 +32,9 @@ export const breakpointStyles = {
     },
 
     diagramItem: {
-      border: "1px solid black",
+      borderTop: "1px solid black",
+      borderRight: "1px solid black",
+      borderLeft: "1px solid black",
       borderBottom: "none",
     },
 
